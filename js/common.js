806260cf7f40ba53998a78bb6eb5e08d301fd475
csh928 <61041001+csh928@users.noreply.github.com>
2020-05-31 17:12:05 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,198 @@
+/*
+    if (navigator.userAgent.indexOf('MSIE') > 0 || navigator.userAgent.indexOf('Trident') > 0) {
+        alert("internet Explorer");
+    }
+    else {
+        alert("Non Ie")
+    }
+*/
+$(document).ready(function(){
+
+
+    $.fn.center = function () {		// 레이어팝업 센터 정렬
+        this.css("position","absolute");
+        this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
+        this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+        return this;
+    }
+
+    $(".Menu").mouseover(function(){
+        $(this).addClass("LiSelected");
+    }).mouseout(function(){
+        $(".Menu").removeClass("LiSelected");
+    }).click(function(){
+        if( $(this).text() == "BACK" ){
+            history.back(-1);
+        };
+        var Mindex = $(this).index() + 1;
+        var pos = $(".outDiv.Num" + Mindex).offset().top - 200;
+
+        $( 'html, body' ).animate( { scrollTop : pos }, 400 );
+    });
+
+
+
+    $(".bg_layer").click(function(){
+        $(".bg_layer,.PopContent").hide();
+        $("body").css("overflow-y","scroll");
+    });
+
+    $( window ).scroll( function() {
+        if ( $(this).scrollTop() > 100 ) {
+            $( '.btn_top' ).fadeIn();
+        } else {
+            $( '.btn_top' ).fadeOut();
+        }
+    } );
+
+    $( '.btn_top' ).click( function() {
+        $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+        return false;
+    } );
+
+    fnSkillSwipe();
+
+});
+
+
+
+function fnMakeSwipe(name, num)
+{
+
+    var html;
+    html = '<div class="swiper-container">';
+    html += ' <div class="swiper-wrapper">';
+    //for (var i of eval(name + num))
+    for (var i = 0; i < eval(name + num).length; i++)
+    {
+        html += '  <div class="swiper-slide">';
+        //for (var j of i )
+        for (var j = 0; j < eval(name + num)[i].length; j++)
+        {
+            //alert(eval(name + num)[i][j]);
+            html += eval(name + num)[i][j];
+        }
+        html += '  </div>';
+    }
+    html += ' </div>';
+    //html += ' <div class="swiper-scrollbar"></div>';
+    html += ' <div class="swiper-pagination"></div>';
+    html += '</div>';
+    html += '<div class="swiper-button-next"></div>';
+    html += '<div class="swiper-button-prev"></div>';
+
+    return html;
+}
+
+function fnSkillSwipe()
+{
+    var html = fnMakeSwipe("SkillContent", "");
+
+    $(".outDiv.Num3").find(".Content").html(html);
+
+    var skillSwiper = new Swiper(".Content>.swiper-container", {
+        //centeredSlides: true,
+        //slidesPerView: '3',
+        spaceBetween: 0,
+        effect: 'slide',
+        //loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: { el: '.swiper-pagination',
+            clickable: true,
+        },
+        //autoplay: {
+        //    delay: 4000,
+        //    disableOnInteraction: false,
+        //},
+    });
+}
+
+
+
+function fnDetail(num)
+{
+    $(".DeContent").innerHeight($(window).innerHeight() - $(".outDiv.MenuDiv").innerHeight());
+    //$(".DeContent").css("padding-top", $(".outDiv.MenuDiv").innerHeight() + 10);
+    var swipe = fnMakeSwipe("PopContent", num);
+    var Title = eval("PopTitle" + num);
+    $(".MMid").append(Title);
+    $(".DeContent").append(swipe);
+
+    var popSwiper = new Swiper(".DeContent>.swiper-container", {
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        effect: 'slide',
+        //loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            //type: 'progressbar',
+        },
+        //scrollbar: {
+            //el: '.swiper-scrollbar',
+            //hide: false,
+        //},
+    });
+}
+
+/*
+function fnPopLayer(num)
+{
+
+    $(".bg_layer").width($(document).width());
+    $(".bg_layer").height($(document).height());
+    $(".PopContent").height($(window).height() * 0.7);
+    //alert(num);
+    var ord = eval("PopDescript" + num);
+
+    var html = fnMakeSwipe("PopContent", num);
+    var PopTitle = '<div class="deTitle">' + ord[0] + '</div>';
+    $(".PopContent").append(PopTitle);
+    $(".PopContent").append(html);
+
+    var Cont = '<div class="descript">';
+    Cont += '  <ul>';
+    for (var i = 1; i < ord.length; i++) {
+        Cont += '<li>' + ord[i] + '</li>'
+    }
+    Cont += '  </ul>';
+    Cont += '</div>';
+
+
+
+
+
+    $(".PopContent").append(Cont);
+
+    $(".bg_layer,.PopContent").show();
+    $(".PopContent").center();
+    $("body").css("overflow-y","hidden");
+
+
+    var popSwiper = new Swiper(".PopContent>.swiper-container", {
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        effect: 'slide',
+        //loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            //clickable: true,
+            type: 'progressbar',
+        },
+    });
+}
+
+*/
---
+++ js/contents.js
@@ -0,0 +1,69 @@
+var SkillContent = [
+    ["<div class='itemBox Bg1'><div class='point'>90%</div></div>","<div class='itemBox Bg2'><div class='point'>85%</div></div>","<div class='itemBox Bg3'><div class='point'>90%</div></div>","<div class='itemBox Bg4'><div class='point'>95%</div></div>","<div class='itemBox Bg5'><div class='point'>95%</div></div>","<div class='itemBox Bg6'><div class='point'>90%</div></div>","<div class='itemBox Bg7'><div class='point'>85%</div></div>"],
+    ["<div class='itemBox Bg8'><div class='point'>70%</div></div>","<div class='itemBox Bg9'><div class='point'>60%</div></div>","<div class='itemBox Bg10'><div class='point'>10%</div></div>","<div class='itemBox Bg11'><div class='point'>30%</div></div>","<div class='itemBox Bg12'><div class='point'>35%</div></div>","<div class='itemBox Bg13'><div class='point'>15%</div></div>"]
+];
+
+var imgPath = "https://user-images.githubusercontent.com/61041001/";
+
+var PopTitle1 = "디자인 공모전";
+var PopContent1 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83326601-4b549680-a2b0-11ea-8adf-bc46986ac84c.jpg"></div>','<div class="explain"><span>탭 이동, 회원 댓글</span></br>[ Jquery, Javascript, Ajax ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83326603-4d1e5a00-a2b0-11ea-9219-645a40aa3b4c.jpg"></div>','<div class="explain"><span>지원자 정보 / 작품설명 / 첨부파일 작성</span></br>[ Jquery, Javascript, Asp, Tabsupload4 ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83326597-498ad300-a2b0-11ea-9fcd-3279ff4700ea.jpg"></div>','<div class="explain"><span>사이트 회원 투표 진행</span></br>[ Jquery, Javascript ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83326604-4d1e5a00-a2b0-11ea-9462-207046c19b5c.jpg"></div>','<div class="explain"><span>사이트 회원 투표 진행</span></br>[ Jquery, Javascript, Asp ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83326602-4b549680-a2b0-11ea-87be-4fc9db030cc2.jpg"></div>','<div class="explain"><span>관리자 사이트 연동 평가 및 메모 저장</span></br>[ Jquery, Javascript, Asp, Oracle ]</div>'],
+];
+
+var PopTitle2 = "ARTBOX MALL";
+var PopContent2 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83343368-5d781880-a334-11ea-83f0-372081da2af7.jpg"></div>','<div class="explain">PC <span>메인 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343787-e5145600-a339-11ea-8dc0-40e582d477c0.jpg"></div>','<div class="explain">MOBILE <span>메인 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343383-9adca600-a334-11ea-9445-f03634f48d39.jpg"><img src="'+imgPath+'83343391-af20a300-a334-11ea-8dbe-9770801667e1.jpg"></div>','<div class="explain"><span>메인 관리자페이지 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Asp, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343710-e1340400-a338-11ea-93b3-9dfcabcfa182.jpg"></div>','<div class="explain">PC <span>상품 상세 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343711-e2653100-a338-11ea-8852-192b8c3acf2e.jpg"></div>','<div class="explain">MOBILE <span>상품 상세 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343721-fb6de200-a338-11ea-8efc-a5149fcb8cb8.jpg"></div>','<div class="explain">PC <span>쿠폰 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343722-fd37a580-a338-11ea-8e35-55b21e1f7358.jpg"></div>','<div class="explain">MOBILE <span>쿠폰 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343735-18a2b080-a339-11ea-8cff-1ac83397bed1.jpg"></div>','<div class="explain">PC <span>마이 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343736-19d3dd80-a339-11ea-8b09-915b1e233283.jpg"></div>','<div class="explain">MOBILE <span>마이 페이지 퍼블리싱 / 데이터베이스 연동</span></span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343741-29ebbd00-a339-11ea-8b86-7206282b290d.jpg"></div>','<div class="explain">PC <span>고객감동센터 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Asp, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343742-2b1cea00-a339-11ea-8c6d-05333caf6956.jpg"></div>','<div class="explain">MOBILE <span>고객감동센터 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Asp, Oracle ]</div>'],
+];
+
+var PopTitle3 = "POOM MOBILE Ver.";
+var PopContent3 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83343898-602a3c00-a33b-11ea-8160-bf0cd43667cc.jpg"></div>','<div class="explain"><span>메인 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, SwipeAPI, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343894-5e607880-a33b-11ea-8131-bf0eb36c44d5.jpg"></div>','<div class="explain"><span>나의 관심 카테고리 퍼블리싱 / 데이터베이스 설계 및 연동</span></br>[ Jquery, Javascript, SortableAPI, Asp, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343917-7c2ddd80-a33b-11ea-9219-2122ec3651f4.jpg"></div>','<div class="explain"><span>나의 관심 카테고리 관리자페이지 / 데이터베이스 연동</span></br>[ Jquery, Javascript, SortableAPI, Asp, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343905-628c9600-a33b-11ea-8f84-2a58afb65e24.jpg"></div>','<div class="explain"><span>카테고리 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343901-615b6900-a33b-11ea-993a-cc1153cd6859.jpg"></div>','<div class="explain"><span>상품리스트 퍼블리싱 / 데이터베이스 연동</span></br>[ 인피니티 스크롤 ] [ Jquery, Javascript, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343903-61f3ff80-a33b-11ea-8116-80b36b6446c8.jpg"></div>','<div class="explain"><span>주문/배송조회 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343904-61f3ff80-a33b-11ea-9109-d339a5fc0f4e.jpg"></div>','<div class="explain"><span>카테고리 상품리스트 퍼블리싱 / 데이터베이스 연동</span></br>각 기획전 최고 할인율 계산 반영</br>[ 반응형 ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343897-5f91a580-a33b-11ea-80b2-0e65f700ecd2.jpg"></div>','<div class="explain"><span>마이 페이지 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343899-60c2d280-a33b-11ea-827e-fb498ea889aa.jpg"></div>','<div class="explain"><span>상품 구매후기 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343900-615b6900-a33b-11ea-9b1b-deefb42dbdf0.jpg"></div>','<div class="explain"><span>구매후기 작성 퍼블리싱 / 데이터베이스 연동</span></br>구매후기 조건에 따른 마일리지 적립</br>[ Jquery, Javascript, Asp, Oracle, Tabsupload4 ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343893-5d2f4b80-a33b-11ea-878b-26eb8d41a69b.jpg"></div>','<div class="explain"><span>히스토리 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343895-5ef90f00-a33b-11ea-9dda-ffb63aea290b.jpg"></div>','<div class="explain"><span>내 회원 등급 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83343896-5ef90f00-a33b-11ea-819d-598bc3ddebc0.jpg"></div>','<div class="explain"><span>로그인 퍼블리싱 / 데이터베이스 연동</br>SNS 로그인</span></br>[ Jquery, Javascript, API, Asp, Oracle ]</div>'],
+];
+
+var PopTitle4 = "LITEM";
+var PopContent4 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83344122-bdbf8800-a33d-11ea-9876-652f628a2646.jpg"></div>','<div class="explain">PC <span>메인 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344123-bf894b80-a33d-11ea-87f4-c985db061e0d.jpg"></div>','<div class="explain">MOBILE <span>메인 퍼블리싱 / 데이터베이스 연동</span></br>[ <span>반응형</span> ] [ Jquery, Javascript, Oracle ]</div>'],
+];
+
+var PopTitle5 = "POOM DIARY";
+var PopContent5 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83344162-1f7ff200-a33e-11ea-8387-ac9fae4054a1.jpg"></div>','<div class="explain">PC <span>다이어리 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344164-2149b580-a33e-11ea-8c26-27f0bd0ade1b.jpg"></div>','<div class="explain">MOBILE <span>다이어리 퍼블리싱 / 데이터베이스 연동</span></br>[ Jquery, Javascript, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344163-2149b580-a33e-11ea-80a7-2df922b95131.jpg"></div>','<div class="explain"><span>관리자 사이트 데이터베이스 설계 및 연동</span></br>[ Jquery, Javascript, Asp, Oracle ]</div>'],
+];
+
+var PopTitle6 = "EVENT PAGE";
+var PopContent6 = [
+    ['<div class="imgBox"><img src="'+imgPath+'83344277-7cc87300-a33f-11ea-903c-995c6ebf6153.jpg"></div>','<div class="explain"><span>날짜별 상품 리스트 변경</br>스케쥴러로 상품 공개여부 변경</span></br>일정날짜, 일정시간에만 상품 구매 가능</br>[ Jquery, Javascript, Ajax, Asp, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344333-2ad41d00-a340-11ea-8aa0-4af4b5110eb3.jpg"></div>','<div class="explain"><span>쿠폰 지급 페이지</br>Ajax 이용</span></br>[ Jquery, Javascript, Asp, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344343-55be7100-a340-11ea-822d-9a415916763a.jpg"></div>','<div class="explain"><span>일정 확률로 이벤트 당첨 프로세서 적용</span></br>[ Jquery, Javascript, Asp, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344365-9322fe80-a340-11ea-9499-41b1c64f7bd8.jpg"></div>','<div class="explain"><span>출석체크 일정 확률로 마일리지 제공</br>전광판 효과</span></br>[ Jquery, Javascript, MarqueeAPI, Ajax, Oracle ]</div>'],
+    ['<div class="imgBox"><img src="'+imgPath+'83344377-cd8c9b80-a340-11ea-83cf-21c5bed795b9.jpg"></div>','<div class="explain"><span>매장정보</span></br>[ Jquery, Javascript, NaverMapAPI, Oracle ]</div>'],
+];
